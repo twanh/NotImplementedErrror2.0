81d222e1eb8b9e01f81d44506f3f0fb04797436b
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-08 13:52:47 +0200

    started work on the username form

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
     /* Header */
-    $page_title = 'Webprogramming Assignment 2';
-    $navigation = Array(
-        'active' => 'Home',
-        'items' => Array(
-        'Home' => '/WP22/assignment_2/index.php',
-        'Links' => '/WP22/assignment_2/links.php',
-        'News' => '/WP22/assignment_2/news.php',
-        'Future' => '/WP22/assignment_2/future.php'
-    )
-    );
+    $page_title = 'Stratego WP22';
     include __DIR__ . '/tpl/head.php';
     /* Body */
     include __DIR__ . '/tpl/body-start.php';
@@ -17,10 +8,32 @@
 <div class="row">
     <div class="col-md-12">
         <h1>Stratego WP22</h1>
+        <?php if(!isset($_POST['name'])) {?>
+        <form action="scripts/main.js" method="post">
+            <div class="form-group">
+                <label for="name">Username</label>
+                <input type="text" id="name" name="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <p>Choose a color below:</p>
+                <div>
+                    <input type="radio" id="red" checked="checked"
+                           name="color" value="red">
+                    <label for="red" style="color: red;">Red</label>
+                    <input type="radio" id="blue"
+                           name="color" value="blue">
+                    <label for="blue" style="color: blue;">Blue</label>
+                </div>
+            </div>
+            <div id="submit" class="btn btn-primary">Start a Game</div>
+        </form>
+        <?php } ?>
     </div>
 </div>
 <?php
-    include __DIR__ . '/tpl/board.php';
+    if(isset($_POST['name'])) {
+        include __DIR__ . '/tpl/board.php';
+    }
     include __DIR__ . '/tpl/body-end.php';
     /* Footer */
     include __DIR__ . '/tpl/footer.php';
