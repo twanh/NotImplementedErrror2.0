397bd2888ea6c1c4001353828e322b3e338f8c63
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-09 14:50:29 +0200

    updated footer

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,6 +1,6 @@
         <footer class="text-center text-white">
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                Stratego made by: Twan Huiskens, Wessel Heerema, Kylian and Koen Snelten
+                Made by: Twan Huiskens, Wessel Heerema, Kylian de Rooij and Koen Snelten
             </div>
         </footer>
     </body>
