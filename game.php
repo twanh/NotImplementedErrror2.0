b40b8cde7fc1a7b8e4d58babb646eeedb028b3ee
Twan H <huiskenstwan@gmail.com>
2022-06-13 14:35:21 +0200

    Ensure that other users cannot join a game if they are not part of the game

--- game.php
+++ game.php
@@ -4,6 +4,27 @@ $page_title = 'Stratego WP22';
 include __DIR__ . '/tpl/head.php';
 /* Body */
 include __DIR__ . '/tpl/body-start.php';
+
+include  __DIR__ . '/db/Database.php';
+
+$userId = $_GET['userid'];
+$gameid = $_GET['gameid'];
+
+$db = new Database('./data/database.json');
+$game = $db->getGameById($gameid);
+
+// Make sure this game exists
+if (is_null($game)) {
+    // TODO: Display a nicer error message!
+    die("This game does not exist! So you cannot play it!");
+}
+
+// Make sure that the current user is player1 or player2
+if ($game['player1Id'] !== $userId and $game['player2Id'] !== $userId) {
+    // TODO: Display a nicer error message!
+    die("It seems like you do not belong in this game");
+}
+
 ?>
     <div class="row my-4">
         <div class="col-md-12 bg-dark rounded">
