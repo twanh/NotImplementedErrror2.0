7fbaf1b88467be71f25b210d1c63cac9522bca77
Koen Snelten <koensnelten@gmail.com>
2022-06-13 11:35:04 +0200

    Some small changes.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <form id="usernameForm" method="post" class="my-4">
             <div class="form-outline form-white mb-4">
                 <label for="name" class="form-label">Username:</label>
-                <input type="text" id="name" name="name" class="form-control" style="color: white;  background-color: rgba(0, 0, 0, 0);" >
+                <input type="text" id="name" name="name" class="form-control">
             </div>
             <div class="form-group mb-4" id="color-tab">
                 <p>Choose a color below:</p>
