06123b1b0b2568991b5d20b1effa7330bf542832
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-13 13:06:07 +0200

    join link generator constructed

--- game.php
+++ game.php
@@ -1,10 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Demo</title>
-</head>
-<body>
-    <h1>this is a test page</h1>
-    <p>check for parameters</p>
-</body>
-</html>
+<?php
+/* Header */
+$page_title = 'Stratego WP22';
+include __DIR__ . '/tpl/head.php';
+/* Body */
+include __DIR__ . '/tpl/body-start.php';
+?>
+    <div class="row my-4">
+        <div class="col-md-12 bg-dark rounded">
+            <h1 class="text-center">Stratego WP22</h1>
+        </div>
+    </div>
+    <div class="row my-4">
+        <div id="urlShare" class="col-md-12 bg-dark rounded">
+            <div id="submit" class="btn btn-primary mt-4">Generate Join Link</div>
+        </div>
+    </div>
+    <script src="scripts/genlink.js"></script>
+<?php
+
+//testing for php and json
+//include __DIR__ . '/scripts/save_to_board.php';
+include __DIR__ . '/tpl/board.php';
+include __DIR__ . '/tpl/body-end.php';
+/* Footer */
+include __DIR__ . '/tpl/footer.php';
+?>
