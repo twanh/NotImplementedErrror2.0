1fecfeb26c8f93e3d62ab242e7ec8bd5ce97945d
kylian3003 <kylianderooij@gmail.com>
2022-06-13 13:47:13 +0200

    added my full name to footer.php

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,6 +1,6 @@
         <footer class="text-center text-white bg-dark fixed-bottom">
             <div class="text-center p-3">
-                Stratego made by: Twan Huiskens, Wessel Heerema, Kylian and Koen Snelten
+                Stratego made by: Twan Huiskens, Wessel Heerema, Kylian de Rooij and Koen Snelten
             </div>
         </footer>
     </body>
