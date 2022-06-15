471e5bfa25d265d21d2a2f075325bcbca1dba2d6
kylian3003 <kylianderooij@gmail.com>
2022-06-15 14:37:28 +0200

    made pieces draggable, dropping doesnt work yet

--- tpl/board.php
+++ tpl/board.php
@@ -4,47 +4,47 @@
             <table id="red-pawns" border="1">
                 <tr id="red-1">
                     <td id="red-1-count">0/1</td>
-                    <td id="red-1-img" class="img-1"></td>
+                    <td id="red-1-img" class="img-1" draggable="true"></td>
                 </tr>
                 <tr id="red-2">
                     <td id="red-2-count">0/1</td>
-                    <td id="red-2-img" class="img-2"></td>
+                    <td id="red-2-img" class="img-2" draggable="true"></td>
                 </tr>
                 <tr id="red-3">
                     <td id="red-3-count">0/2</td>
-                    <td id="red-3-img" class="img-3"></td>
+                    <td id="red-3-img" class="img-3" draggable="true"></td>
                 </tr>
                 <tr id="red-4">
                     <td id="red-4-count">0/3</td>
-                    <td id="red-4-img" class="img-4"></td>
+                    <td id="red-4-img" class="img-4" draggable="true"></td>
                 </tr>
                 <tr id="red-5">
                     <td id="red-5-count">0/4</td>
-                    <td id="red-5-img" class="img-5"></td>
+                    <td id="red-5-img" class="img-5" draggable="true"></td>
                 </tr>
                 <tr id="red-6">
                     <td id="red-6-count">0/4</td>
-                    <td id="red-6-img" class="img-6"></td>
+                    <td id="red-6-img" class="img-6" draggable="true"></td>
                 </tr>
                 <tr id="red-7">
                     <td id="red-7-count">0/4</td>
-                    <td id="red-7-img" class="img-7"></td>
+                    <td id="red-7-img" class="img-7" draggable="true"></td>
                 </tr>
                 <tr id="red-8">
                     <td id="red-8-count">0/5</td>
-                    <td id="red-8-img" class="img-8"></td>
+                    <td id="red-8-img" class="img-8" draggable="true"></td>
                 </tr>
                 <tr id="red-9">
                     <td id="red-9-count">0/8</td>
-                    <td id="red-9-img" class="img-9"></td>
+                    <td id="red-9-img" class="img-9" draggable="true"></td>
                 </tr>
                 <tr id="red-spy">
                     <td id="red-spy-count">0/1</td>
-                    <td id="red-spy-img" class="img-S"></td>
+                    <td id="red-spy-img" class="img-S" draggable="true"></td>
                 </tr>
                 <tr id="red-bomb">
                     <td id="red-bomb-count">0/6</td>
-                    <td id="red-bomb-img" class="img-B"></td>
+                    <td id="red-bomb-img" class="img-B" draggable="true"></td>
                 </tr>
             </table>
         </div>
@@ -175,47 +175,47 @@
         <div class="col-md-2 d-flex justify-content-start">
             <table id="blue-pawns" border="1">
                 <tr id="blue-1">
-                    <td id="blue-1-img"  class="img-1"></td>
+                    <td id="blue-1-img"  class="img-1" draggable="true"></td>
                     <td id="blue-1-count">0/1</td>
                 </tr>
                 <tr id="blue-2">
-                    <td id="blue-2-img" class="img-2"></td>
+                    <td id="blue-2-img" class="img-2" draggable="true"></td>
                     <td id="blue-2-count">0/1</td>
                 </tr>
                 <tr id="blue-3">
-                    <td id="blue-3-img" class="img-3"></td>
+                    <td id="blue-3-img" class="img-3" draggable="true"></td>
                     <td id="blue-3-count">0/2</td>
                 </tr>
                 <tr id="blue-4">
-                    <td id="blue-4-img" class="img-4"></td>
+                    <td id="blue-4-img" class="img-4" draggable="true"></td>
                     <td id="blue-4-count">0/3</td>
                 </tr>
                 <tr id="blue-5">
-                    <td id="blue-5-img" class="img-5"></td>
+                    <td id="blue-5-img" class="img-5" draggable="true"></td>
                     <td id="blue-5-count">0/4</td>
                 </tr>
                 <tr id="blue-6">
-                    <td id="blue-6-img" class="img-6"></td>
+                    <td id="blue-6-img" class="img-6" draggable="true"></td>
                     <td id="blue-6-count">0/4</td>
                 </tr>
                 <tr id="blue-7">
-                    <td id="blue-7-img" class="img-7"></td>
+                    <td id="blue-7-img" class="img-7" draggable="true"></td>
                     <td id="blue-7-count">0/4</td>
                 </tr>
                 <tr id="blue-8">
-                    <td id="blue-8-img" class="img-8"></td>
+                    <td id="blue-8-img" class="img-8" draggable="true"></td>
                     <td id="blue-8-count">0/5</td>
                 </tr>
                 <tr id="blue-9">
-                    <td id="blue-9-img" class="img-9"></td>
+                    <td id="blue-9-img" class="img-9" draggable="true"></td>
                     <td id="blue-9-count">0/8</td>
                 </tr>
                 <tr id="blue-spy">
-                    <td id="blue-spy-img" class="img-S"></td>
+                    <td id="blue-spy-img" class="img-S" draggable="true"></td>
                     <td id="blue-spy-count">0/1</td>
                 </tr>
                 <tr id="blue-bomb">
-                    <td id="blue-bomb-img" class="img-B"></td>
+                    <td id="blue-bomb-img" class="img-B" draggable="true"></td>
                     <td id="blue-bomb-count">0/6</td>
                 </tr>
             </table>
