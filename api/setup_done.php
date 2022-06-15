25252f9cc0ddaf0ccc2da7977e2c9a3384459f42
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-15 15:00:31 +0200

    rudimentary board validation

--- api/setup_done.php
+++ api/setup_done.php
@@ -4,12 +4,39 @@ require __DIR__ . '/../classes/Board.php';
 require __DIR__ . '/../db/Database.php';
 
 if($_POST["red-ready"] === 'true' && $_POST["blue-ready"] === 'true' && isset($_POST["gameid"])) {
-    $board = getBoard($_POST["gameid"]);
+    $database = new Database('../data/database.json');
+    $board = $database->getBoard($_POST["gameid"]);
+    $correct = true;
     for ($y = 0; $y < 10; $y++) {
-        if ($board[$y]) {
-            echo("foobar");
+        $count_filled = 0;
+        for ($x = 0; $x < 10; $x++) {
+            if ($board[$y][$x] !== NULL || $board[$y][$x] !== "WATER") {
+                $count_filled = $count_filled + 1;
+            }
+        }
+        if (($y < 4 || $y > 5) && $count_filled !== 10) {
+            $correct = false;
+        } else if (($y === 4 || $y === 5) && $count_filled !== 0) {
+            $correct = false;
         }
     }
+    if ($correct) {
+        $data = [
+            "success" => true,
+            "message" => "Pieces OK: Ready to play",
+        ];
+    } else {
+        $data = [
+            "success" => false,
+            "message" => "Invalid amount of pieces!",
+        ];
+    }
 } else {
-    echo("Not all players are ready yet!");
+    $data = [
+        "success" => false,
+        "message" => "Not all players are ready yet!",
+    ];
 }
+header('Content-Type: application/json');
+echo json_encode($data);
+die();
