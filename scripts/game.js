2b5ffc3fe7af1c334f59d99e1dabc6aa4a6b7f43
Twan H <huiskenstwan@gmail.com>
2022-06-20 13:50:48 +0200

    Add api route and javascript function to get the user info of a player

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,37 @@
 
+async function getUserInfo(gameid, userid) {
 
+    let req = $.ajax({
+        url: 'api/player.php',
+        method: "POST",
+        data: {
+            gameid,
+            userid,
+        },
+        dataType: 'json',
+    });
+
+    const data = await req.done((data) => {
+        return data;
+    });
+
+    return data;
+}
+
+function getCurrentUserInfo() {
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const gameid = urlParams.get('gameid');
+    const userid = urlParams.get('userid');
+
+    const userinfo = getUserInfo(gameid, userid).then((data) => {
+        return data;
+    });
+
+    return userinfo;
+
+}
 
 function getPlayerPieces() {
 
