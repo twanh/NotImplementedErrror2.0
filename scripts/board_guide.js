3f8784682a3e877ce03998346d8ba40fc86ce9a3
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-22 14:40:22 +0200

    made board_guide.js fancier

--- scripts/board_guide.js
+++ scripts/board_guide.js
@@ -1,24 +1,26 @@
 function highlightSide(rowstart, color) {
     for (let i = rowstart; i < 4 + rowstart; i++) {
         let rows = $("#row-"+i.toString()).children();
-        console.log(rows)
         rows.css("background-color", color);
+    }
+    let inverse = (rowstart + 4) % 10;
+    for (let i = inverse; i < 6 + inverse; i++) {
+        let rows = $("#row-"+i.toString()).children();
+        rows.css("background-color", "rgba(0, 0, 0, 0.5)");
     }
 }
 
 async function tailorToPlayer() {
-    console.log("Function tailorToPlayer active");
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const gameid = urlParams.get('gameid');
     const userid = urlParams.get('userid');
     const info = await getUserInfo(gameid, userid);
     const color = info["color"];
-    console.log(color);
     if (color === "red") {
-        highlightSide(7, "rgba(255, 153, 153, 0.5)");
+        highlightSide(7, "rgba(255, 153, 153, 0.2)");
     } else if (color === "blue") {
-        highlightSide(1, "rgba(153, 153, 255, 0.5)");
+        highlightSide(1, "rgba(153, 153, 255, 0.2)");
     }
 }
 
