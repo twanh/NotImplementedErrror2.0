e0c8af56fafb13f0ad0a71ac53675e130423d62a
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-22 13:29:22 +0200

    refactored game.php to lobby.php

--- scripts/genlink.js
+++ scripts/genlink.js
@@ -1,5 +1,5 @@
 function generateLink() {
-    let newUrl = window.location.href.replace("game.php", "join.php").split("&")[0];
+    let newUrl = window.location.href.replace("lobby.php", "join.php").split("&")[0];
     $("#urlShare").append('<div class="input-group mb-4"><input type="text" class="form-control" id="myLink" value="' + newUrl + '" readonly><div class="input-group-append"><button onclick="copyLink()" class="btn btn-primary">Copy</button></div></div>');
 }
 
