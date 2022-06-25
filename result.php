5ea1f869b0d7409ccab69d8557a7b8dbe2aefdb1
WHeerema-RUG <w.heerema@student.rug.nl>
2022-06-25 15:28:09 +0200

    changed victory_screen.php into result.php and added very rudimentary check

--- result.php
+++ result.php
@@ -1,13 +1,18 @@
 <?php
     /* Header */
-    $page_title = 'Stratego WP22 - Create Game';
+    $is_won = $_GET["winner"];
+    if ($is_won == "true") {
+        $page_title = 'Stratego WP22 - You win!';
+    } else {
+        $page_title = 'Stratego WP22 - You lose...';
+    }
     include __DIR__ . '/tpl/head.php';
     /* Body */
     include __DIR__ . '/tpl/body-start.php';
 ?>
 <div class="row my-4">
     <div class="col-md-12 bg-dark rounded">
-        <!-- if player wins show win screen -->
+        <?php if ($is_won == "true") { ?>
         <svg viewBox="0 0 500 100">
             <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
             <text width="500">
@@ -16,9 +21,9 @@
                 </textPath>
             </text>
         </svg>
-        <h4 class="my-4">After a heroic battle your troops have won! You can sit still or battle on!</h4>
-        <a href="/" class="btn btn-success btn-block">Battle on!</a>
-        <!-- if player loses show win screen -->
+        <h4 class="my-4">After a heroic battle, your troops have won! However, victory never last forever, so battle on!</h4>
+        <a href="index.php" class="btn btn-success btn-block">Battle on!</a>
+        <?php } else { ?>
         <svg viewBox="0 0 500 100">
             <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
             <text width="500">
@@ -27,8 +32,9 @@
                 </textPath>
             </text>
         </svg>
-        <h4 class="my-4">After a hard defeat your troops have lost! You can still try and reclaim your honor and battle on!</h4>
-        <a href="/" class="btn btn-success btn-block">Battle on!</a>
+        <h4 class="my-4">A crushing defeat, your troops have lost! However, you can still reclaim your honor, so battle on!</h4>
+        <a href="index.php" class="btn btn-success btn-block">Battle on!</a>
+        <?php } ?>
     </div>
 </div>
 <?php
