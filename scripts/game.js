970c5d408769fe18b6fb6fe4a0e97fdb65a8e499
Twan H <huiskenstwan@gmail.com>
2022-06-27 13:17:00 +0200

    Add jsdoc strings

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,10 @@
-
+/**
+ * Gets the users information.
+ *
+ * @param  {} gameid The id of the current game.
+ * @param  {} userid The id of the current user.
+ * @returns {Promise<Object>} The user information (as promise).
+ */
 async function getUserInfo(gameid, userid) {
 
     let req = $.ajax({
@@ -17,7 +23,13 @@ async function getUserInfo(gameid, userid) {
 
     return data;
 }
-
+/**
+ * Returns the user inforamtion of the user.
+ *
+ * Note: The userid and gameid are found in the url.
+ *
+ * @returns {Promise<Object>} The user information.
+ */
 function getCurrentUserInfo() {
 
     const queryString = window.location.search;
@@ -32,7 +44,12 @@ function getCurrentUserInfo() {
     return userinfo;
 
 }
-
+/**
+ * Checks if it is the current user's turn to make a move.
+ *
+ * @returns {Promise<boolean>} If it is the current user's turn.
+ *
+ */
 async function checkIsTurn() {
 
     const queryString = window.location.search;
@@ -58,7 +75,14 @@ async function checkIsTurn() {
 
 
 }
-
+/**
+ * Get's the pieces of the current player.
+ *
+ * Note: the pieces of the other player are shown as "UNKNOWN".
+ *
+ * @returns {Promise<Array<Array<Object|string|null>>>} The board with the other players pieces redacted.
+ *
+ */
 function getPlayerPieces() {
 
     const queryString = window.location.search;
@@ -84,7 +108,15 @@ function getPlayerPieces() {
 
 }
 
-
+/**
+ * Makes a move.
+ *
+ * @param {number} cur_y The current y position of the piece to move.
+ * @param {number} cur_x The current x position of the piece to move.
+ * @param {string} direction The direction ('up','down','left','right') to move the piece in.
+ * @param {number} distance=1 The distance to move the piece with.
+ * @returns {Promise<Object>} The return value from the server.
+ */
 function move(cur_y, cur_x, direction, distance = 1){
 
     const queryString = window.location.search;
